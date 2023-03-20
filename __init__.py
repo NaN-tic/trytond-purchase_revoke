b549691dc5933f40ebb6661704fc671bfc27b78a
Raimon Esteve <raimonesteve@gmail.com>
2023-03-20 09:47:07 +0100

    Purchase Revoke
    #072950

--- __init__.py
+++ __init__.py
@@ -2,11 +2,12 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.pool import Pool
+from . import purchase
 
 def register():
     Pool.register(
+        purchase.Purchase,
         module='purchase_revoke', type_='model')
     Pool.register(
+        purchase.PurchaseCreatePendingMoves,
         module='purchase_revoke', type_='wizard')
-    Pool.register(
-        module='purchase_revoke', type_='report')
